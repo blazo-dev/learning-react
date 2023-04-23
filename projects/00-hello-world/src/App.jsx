e57688b506b6ddb406d2bb57365158501685baac
Bryan Lazo <lebry4n@gmail.com>
2023-04-23 13:11:54 -0600

    Creating TwitterFollowCard component

---
+++ projects/00-hello-world/src/App.jsx
@@ -0,0 +1,18 @@
+import { TwitterFollowCard } from "./TwitterCard/TwitterFollowCard";
+import { usersMocks } from "./TwitterCard/mocks/UsersMocks";
+
+export const App = () => {
+  return (
+    <div className="app">
+      {usersMocks.map(({ id, userName, initialIsFollowing, name }) => (
+        <TwitterFollowCard
+          key={id}
+          userName={userName}
+          initialIsFollowing={initialIsFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
+    </div>
+  );
+};
---
+++ projects/00-hello-world/src/TwitterCard/mocks/UsersMocks.js
@@ -0,0 +1,62 @@
+export const usersMocks = [
+  {
+    id: 1,
+    name: "Bryan Lazo",
+    userName: "newlaz",
+    initialIsFollowing: true,
+  },
+  {
+    id: 2,
+    name: "Miguel Angel Duran",
+    userName: "midudev",
+    initialIsFollowing: true,
+  },
+  {
+    id: 3,
+    name: "Carlos",
+    userName: "carlitos",
+    initialIsFollowing: false,
+  },
+  {
+    id: 4,
+    name: "Luis",
+    userName: "luisillo",
+    initialIsFollowing: false,
+  },
+  {
+    id: 5,
+    name: "Ana",
+    userName: "anita",
+    initialIsFollowing: false,
+  },
+  {
+    id: 6,
+    name: "Pedro",
+    userName: "pedrito",
+    initialIsFollowing: false,
+  },
+  {
+    id: 7,
+    name: "Elena",
+    userName: "elenita",
+    initialIsFollowing: false,
+  },
+  {
+    id: 8,
+    name: "Miguel",
+    userName: "miguelito",
+    initialIsFollowing: false,
+  },
+  {
+    id: 9,
+    name: "Laura",
+    userName: "laurita",
+    initialIsFollowing: false,
+  },
+  {
+    id: 10,
+    name: "Sofia",
+    userName: "sofilita",
+    initialIsFollowing: false,
+  },
+];
